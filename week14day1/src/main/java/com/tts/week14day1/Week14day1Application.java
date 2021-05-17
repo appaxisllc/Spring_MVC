0cb9cf76fe7c5d9034b881f6ef2f079a03b0d975
appaxisllc <68441201+appaxisllc@users.noreply.github.com>
2021-05-17 11:52:25 -0400

    initial upload
    MVC week 14 day 1

---
+++ week14day1/src/main/java/com/tts/week14day1/Week14day1Application.java
@@ -0,0 +1,15 @@
+package com.tts.week14day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Week14day1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week14day1Application.class, args);
+	}
+
+
+}
